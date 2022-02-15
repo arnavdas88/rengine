92c9419b02c44eced56c942b1832adc21993ae7a
Arnav Das <arnav.das88@gmail.com>
2022-02-15 21:49:58 +0000

    duplicate path removed
    Signed-off-by: Arnav Das <arnav.das88@gmail.com>

--- web/api/urls.py
+++ web/api/urls.py
@@ -67,10 +67,6 @@ urlpatterns = [
         ListDorkTypes.as_view(),
         name='queryDorkTypes'),
     path(
-        'queryDorkTypes/',
-        ListDorkTypes.as_view(),
-        name='queryDorkTypes'),
-    path(
         'queryAllScanResultVisualise/',
         VisualiseData.as_view(),
         name='queryAllScanResultVisualise'),
